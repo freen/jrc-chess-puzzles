0b7a6ea6447429025aaf3d13331dff58c4c61ba1
freen <dbickett@gmail.com>
2013-03-24 15:34:02 -0400

    puzzles successfully converting to base FEN notation

--- jrc-parser-ruby/lib/jrc-parser.rb
+++ jrc-parser-ruby/lib/jrc-parser.rb
@@ -15,6 +15,7 @@ class JrcParser
     markup = get_page_markup(@page_uri)
     @doc = Nokogiri::HTML(markup)
     puzzles = get_puzzles(@doc)
+    puts puzzles
   end
 
   private
@@ -33,10 +34,56 @@ class JrcParser
     end
 
     def get_puzzles(doc)
-      imgs = doc.xpath('//img').select { |img| img.attr('src').match(/[a-z]{2}_[dl]\.gif/) }
+      imgs = doc.xpath('//img').select do |img|
+        img.attr('src').match(/[a-z]{2}_[dl]\.gif/)
+      end
       boards = imgs.each_slice(64).to_a
-      boards.each do |board|
+      boards.map do |board|
+        board = board_to_fen(board)
+      end
+    end
+
+    # Converts an array of 64 Nokogiri <img /> elements to Forsyth-Edwards Notation
+    def board_to_fen(board)
+      as_fen = ""
+      ranks = board.each_slice(8).to_a
+      ranks.each_with_index do |rank, index|
+        as_fen << rank_to_fen(rank)
+        as_fen << "/" if index < 7
+      end
+      as_fen
+    end
+
+    def rank_to_fen(rank)
+      as_fen = ""
+      blank_counter = 0;
+      rank.each_with_index do |img, index|
+        src = img.attr("src");
+
+        # Blank square
+        if src.match(/sq_[ld]\.gif/)
+          blank_counter += 1
+          as_fen << blank_counter.to_s if 7 == index
+
+        # Square with a piece
+        elsif src.match(/([wb])([rnbqkp])_[ld]\.gif/)
+          if blank_counter > 0
+            # Add blank spaces and reset blank counter
+            as_fen << blank_counter.to_s
+            blank_counter = 0
+          end
+          # Add the discovered space
+          color = Regexp.last_match(1) # one of: wb
+          piece = Regexp.last_match(2) # one of: rnbqkp
+          case color
+          when "w"
+            as_fen << piece.upcase
+          when "b"
+            as_fen << piece
+          end
+        end
 
       end
+      as_fen
     end
 end
